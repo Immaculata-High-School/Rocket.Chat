67c95938a32cbd6959baedf31496c8e792cef057
Ricardo Garim <rswarovsky@gmail.com>
2025-11-24 15:18:54 -0300

    refactor: change invited prop name to status

--- ee/packages/federation-matrix/src/FederationMatrix.ts
+++ ee/packages/federation-matrix/src/FederationMatrix.ts
@@ -918,7 +918,7 @@ export class FederationMatrix extends ServiceClass implements IFederationMatrixS
 			{
 				'_id': subscriptionId,
 				'u._id': userId,
-				'invited': true,
+				'status': 'INVITED',
 			},
 			{ projection: { _id: 1, rid: 1, federation: 1 } },
 		);
